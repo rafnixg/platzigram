f749546d56237e5d99b809ff566c2f7a086ac979
Rafnix Guzman <rafnixg@gmail.com>
2019-09-04 12:48:53 -0500

    [MOD] Config the Users(Profile) app

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # Local apps
     'posts',
+    'users',
 
 ]
 
@@ -123,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
